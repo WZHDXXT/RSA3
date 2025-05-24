c772569c2e25fd57dda48efad3a6b89660e5e4a2
Jiaxuan Yu <166388707+WZHDXXT@users.noreply.github.com>
2025-05-24 23:30:28 +0200

    Add files via upload

--- models/item_encoder.py
+++ models/item_encoder.py
@@ -7,15 +7,13 @@ class ItemEncoder(nn.Module):
         self.category_embed = nn.Embedding(num_categories + 1, 16)
         self.store_embed = nn.Embedding(num_stores + 1, 16)
         self.parent_asin_embed = nn.Embedding(num_parent_asin + 1, 16)
-        self.text_fc = nn.Linear(text_embedding_dim, 64)
-        self.numeric_fc = nn.Linear(3, 16)
-        self.output_fc = nn.Linear(16 + 16 + 16 + 64 + 16, 128)
+        self.text_fc = nn.Linear(text_embedding_dim, 64)  # text_embedding 替代 title
+        self.output_fc = nn.Linear(16 + 16 + 16 + 64, 128)
 
-    def forward(self, category, store, parent_asin, text_embedding, numeric_feats):
+    def forward(self, category, store, parent_asin, text_embedding):
         cat_emb = self.category_embed(category)
         store_emb = self.store_embed(store)
         parent_emb = self.parent_asin_embed(parent_asin)
         text_feat = self.text_fc(text_embedding)
-        numeric_feat = self.numeric_fc(numeric_feats)
-        x = torch.cat([cat_emb, store_emb, parent_emb, text_feat, numeric_feat], dim=-1)
+        x = torch.cat([cat_emb, store_emb, parent_emb, text_feat], dim=-1)
         return self.output_fc(x)
--- models/user_encoder.py
+++ models/user_encoder.py
@@ -8,6 +8,8 @@ class UserEncoder(nn.Module):
         self.gru = nn.GRU(input_size=item_embedding.embedding_dim, hidden_size=128, batch_first=True)
 
     def forward(self, item_seq):
+        if item_seq.dim() == 3 and item_seq.size(-1) == 1:
+            item_seq = item_seq.squeeze(-1)
         embed_seq = self.item_embedding(item_seq)  # (B, T, E)
         _, h = self.gru(embed_seq)  # h: (1, B, H)
         return h.squeeze(0)  # (B, H)
